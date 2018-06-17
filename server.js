199e6fa5972950c3a5c37d56c1ed3281c19196cb
Kimberly Warner <kimberlyc222@outlook.com>
2018-06-16 19:27:55 -0700

    reconfigured for blog api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
+const {BlogPosts} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,59 +12,24 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
+// we're going to add some items to BlogPosts
 // so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
-
-// adding some recipes to `Recipes` so there's something
-// to retrieve.
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+BlogPosts.create('Lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+BlogPosts.create('Curabitur', 'Curabitur tortor. Pellentesque nibh. Aenean quam.');
+BlogPosts.create('Quisque', 'Quisque volutpat condimentum velit. Class torquent per conubia nostra, per inceptos himenaeos.');
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+app.get('/blogposts', (req, res) => {
+  res.json(BlogPosts.get());
 });
 
-app.post('/shopping-list', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
-});
-
-app.delete('/shopping-list/:id', (req, res) => {
-  ShoppingList.delete(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.id}\``);
-  res.status(204).end();
-});
-
-app.delete('/recipes/:id', (req, res) => {
-  Recipes.delete(req.params.id);
-  console.log(`Deleted recipe item \`${req.params.id}\``);
-  res.status(204).end();
-});
-
-// when new recipe added, ensure has required fields. if not,
+// when new blogpost added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
-app.post('/recipes', jsonParser, (req, res) => {
+app.post('/blogposts', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
-  const requiredFields = ['name', 'ingredients'];
+  const requiredFields = ['title', 'article'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -73,14 +38,16 @@ app.post('/recipes', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = Recipes.create(req.body.name, req.body.ingredients);
+
+  const item = BlogPosts.create(req.body.title, req.body.article);
   res.status(201).json(item);
 });
 
-
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-})
+app.delete('/blogposts/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted blogpost item \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
